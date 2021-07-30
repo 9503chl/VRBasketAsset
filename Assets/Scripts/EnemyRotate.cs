ca0e290e65807b006709ccee17655384877c3bf5
9503chl <9503chl@naver.com>
2021-07-30 14:34:03 +0900

    '21.07.30.2'

--- Assets/Scripts/EnemyRotate.cs
+++ Assets/Scripts/EnemyRotate.cs
@@ -8,20 +8,21 @@ public class EnemyRotate : MonoBehaviour
     float x1;
     Vector3 movement;
     Vector3 position;
-
+    public Transform targetTransform; //다른 오브젝트 가져오는 쉬운방법
+    Rigidbody rigid;
     void Awake()
     {
-        GetComponent<Rigidbody>();  
+        rigid = GetComponent<Rigidbody>();  
     }
 
     void Update()
     {
         transform.Translate(Vector3.forward * 0.002f); //가는방향
         time += Time.deltaTime;
-        transform.rotation = Quaternion.LookRotation(GameObject.Find("GoalDae").GetComponent<Transform>().position);
-
-        if (time >= 1.5f)
+        transform.LookAt(targetTransform);
+        if (time >= 2.0f)
         {
+            rigid.AddForce(Vector3.up , ForceMode.Impulse);
             time = 0.0f;
         }
     }
