0d80ce6a4dae8f9e1909a2c0b8ac5b42fd447d30
9503chl <9503chl@naver.com>
2021-08-04 17:29:12 +0900

    '21.08.04'

---
+++ Assets/Scripts/ButtonWait.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonWait : MonoBehaviour
+{
+    float time = 2.0f;
+
+    void Update()
+    {
+        time -= Time.deltaTime;
+        if(time <= 0.0f)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/CloseUp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+[RequireComponent(typeof(RectTransform))]
+public class CloseUp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var rectTranform = GetComponent<RectTransform>();
+
+        var sequence = DOTween.Sequence();
+        transform.localScale = Vector3.zero;
+
+        sequence.Append(transform.DOScale(1, 1).SetEase(Ease.OutBounce));
+
+        sequence.Join(GetComponent<CanvasGroup>().DOFade(1, 1));
+    }
+}
