0dcb7f6709548138b145710a09599f53c5e1e7dc
9503chl <9503chl@naver.com>
2021-08-18 11:22:18 +0900

    '21.08.18'

---
+++ Assets/ControllerInput.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControllerInput : MonoBehaviour
+{
+    public Text InputText;
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger)) //트리거 당겼을때
+        {
+            InputText.text = "Trigger";
+        }
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryTouchpad))
+        {
+            Vector2 InputPoint = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad);
+            InputText.text = InputPoint.ToString();
+        }
+    }
+}
